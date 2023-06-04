ca0c4d961508546d06ed7269246eb20a5e1477ce
Sarmad Rehman <sarmadr02@gmail.com>
2023-06-04 20:03:21 +0500

    basic layout and assets provided

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
+
+const { width, height } = Dimensions.get("window");
+
 const MusicPlayer = () => {
   return (
-    <View style={style.container}>
-      <Ionicons name="heart-outline" size={30} />
-      <Text>Music Player</Text>
-    </View>
+    <SafeAreaView style={style.container}>
+      <View style={style.maincontainer}>{/* FOR OTHER CONTENTS */}</View>
+
+      <View style={style.bottomcontainer}>
+        <View style={style.bottomIconWrapper}>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="heart-outline" size={30} color="#888888" />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="repeat" size={30} color="#888888" />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="share-outline" size={30} color="#888888" />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="ellipsis-horizontal" size={30} color="#888888" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default MusicPlayer;
+
 const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#555",
     alignItems: "center",
     justifyContent: "center",
+  },
+  maincontainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bottomcontainer: {
+    width: width,
+    alignItems: "center",
+    paddingVertical: 15,
+    borderTopColor: "#393e46",
+    borderWidth: 1,
+  },
+  bottomIconWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
   },
 });
